5c72084152a6ec80b3b5e56a81c94b0bc404ed64
AmandaMWilliams <73121365+AmandaMWilliams@users.noreply.github.com>
2020-11-27 15:53:01 -0500

    this doesnt work

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -4,11 +4,27 @@ public class Music {
 
     private String[] playList;
 
-    public Music(String[] playList){
+    public Music(String[] playList) {
+
         this.playList = playList;
     }
 
-    public Integer selection(Integer startIndex, String selection){
-        return null;
+    public Integer selection(Integer startIndex, String selection) {
+        int forwardCount = 0;
+        int backwardCount = 0;
+
+        for (int i = 0; i < playList.length; i++) {
+            if (playList[startIndex].equals(selection)) {
+                return 0;
+            } else {
+                forwardCount++;
+                backwardCount--;
+            }
+        }
+
+        if (forwardCount > Math.abs(backwardCount)) {
+            return Math.abs(backwardCount);
+        }
+        return forwardCount;
     }
 }
